66d42802c133deebfc09795546cb673c784efaa2
onixldlc <onxdl16427@outlook.com>
2022-10-02 17:13:42 +0700

    added script for easy setup and compile, and fix some error

--- setup.sh
+++ setup.sh
@@ -1,11 +1,32 @@
+#!/bin/bash
 
+SCRIPT_DIR=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &> /dev/null && pwd)
 
+function rename ()
+{
+  ! [ -f "${2}" ] && cp "${1}" "${2}" || echo "${2} already exist"
+}
 
 function renameAll ()
 {
   for f in ${1}; do
-    mv -- "${f${2}}"
+    rename "${f}" "${f%.example}"
   done
 }
 
+function goHome()
+{
+  cd "${SCRIPT_DIR}"
+}
 
+function absolutePath ()
+{
+  goHome
+  cd "${1}"
+}
+
+absolutePath "./cv/"
+renameAll "$(ls -1)"
+
+absolutePath "./component/"
+rename "personal-data.tex.example" "personal-data.tex"
